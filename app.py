2892e68578728694a8cd69df45678755fa7c3a27
Tamb1812301 <tamb1812301@student.ctu.edu.vn>
2021-09-06 21:08:50 +0700

    webapp

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import joblib
-import pandas as pb
 import numpy as np
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
